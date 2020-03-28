60e0166a7a4f67cf8f7203b0e17a242a03d9d294
kubi <pietrusjakub@gmail.com>
2020-03-28 23:37:24 +0100

    Created Fake identities, using builder model

---
+++ WebStore.Tests/Controller/AccountControllerShould.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebStore.Controllers;
+using WebStore.Tests.FakeClasses.Identity;
+using WebStore.ViewModels;
+using Xunit;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace WebStore.Tests.Controller
+{
+    public class AccountControllerShould : StoreTestBase
+    {
+        private readonly Mock<FakeUserManager>_mockUserManager;
+        private readonly Mock<FakeSignInManager> _mockSignInManager;
+        private readonly AccountController _sut;
+
+
+        public AccountControllerShould()
+        {
+            _mockUserManager = new FakeUserManagerBuilder().Build();
+            _mockSignInManager = new FakeSignInManagerBuilder()
+                .With(x => x.Setup(sm => sm.PasswordSignInAsync(It.IsAny<string>(),
+                        It.IsAny<string>(),
+                        It.IsAny<bool>(),
+                        It.IsAny<bool>()))
+                    .ReturnsAsync(SignInResult.Success))
+                .Build();
+            _sut = new AccountController(_context, _mockUserManager.Object, _mockSignInManager.Object);
+        }
+
+
+        [Fact]
+        public void ReturnViewForRegister()
+        {
+            //Act
+            IActionResult result = _sut.Register();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+
+        [Fact]
+        public async void RedirectUserAfterSuccesfoulLogin()
+        {
+            var mockUrlHelper = new Mock<IUrlHelper>(MockBehavior.Strict);
+            mockUrlHelper
+                .Setup(x => x.IsLocalUrl(It.IsAny<string>()))
+                .Returns(true)
+                .Verifiable();
+            _sut.Url = mockUrlHelper.Object;
+            var result = await _sut.Login(new LoginViewModel(), "testPath");
+            Assert.IsType<RedirectResult>(result);
+        }
+
+    }
+}
---
+++ WebStore.Tests/FakeClasses/Identity/FakeSignInManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using WebStore.Models;
+
+namespace WebStore.Tests.FakeClasses.Identity
+{
+    public class FakeSignInManager : SignInManager<User>
+    {
+        public FakeSignInManager()
+            : base(userManager: new Mock<FakeUserManager>().Object,
+                contextAccessor: new HttpContextAccessor(),
+                claimsFactory: new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+                optionsAccessor: new Mock<IOptions<IdentityOptions>>().Object,
+                logger: new Mock<ILogger<SignInManager<User>>>().Object,
+                schemes: new Mock<IAuthenticationSchemeProvider>().Object,
+                new DefaultUserConfirmation<User>())
+        { }
+    }
+}
